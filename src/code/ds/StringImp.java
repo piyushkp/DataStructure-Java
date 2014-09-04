5e89fc98361688220d1814f6da9880ffc0030fed
Piyush <er.piyushpatel@gmail.com>
2014-09-04 09:07:33 -0700

    updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -4,46 +4,41 @@ import java.util.*;
 /**
  * Created by Piyush Patel.
  */
-public class StringImp
-{
+public class StringImp {
     /* Compress a given string. Input: aaaaabbccc  Output: a5b2c3    */
-    static void compressString(String s){
+    static void compressString(String s) {
         char[] string = s.toCharArray();
-        if (string.length==0) return;
+        if (string.length == 0) return;
         char first = string[0];
         int count = 0;
-        for(int i=0;i<string.length;i++){
-            if(first!=string[i]){
-                System.out.printf("%c%d",first,count);
-                count=1;
-                first=string[i];
-            }else{
+        for (int i = 0; i < string.length; i++) {
+            if (first != string[i]) {
+                System.out.printf("%c%d", first, count);
+                count = 1;
+                first = string[i];
+            } else {
                 count++;
             }
         }
-        System.out.printf("%c%d",first,count);
+        System.out.printf("%c%d", first, count);
     }
 
     //Finds first non repeated character in a String in just one pass.
-    private char firstNonRepeatingChar(String word)
-    {   Set<Character> repeating = new HashSet<Character>();
+    private char firstNonRepeatingChar(String word) {
+        Set<Character> repeating = new HashSet<Character>();
         List<Character> nonrepeating = new ArrayList<Character>();
-        for ( int i= 0; i< word.length();i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
-            if(repeating.contains(letter))
+            if (repeating.contains(letter))
                 continue;
-            if(nonrepeating.contains(letter))
-            {
+            if (nonrepeating.contains(letter)) {
                 nonrepeating.remove(letter);
                 repeating.add(letter);
-            }
-            else
-            {
+            } else {
                 nonrepeating.add(letter);
             }
         }
-    return nonrepeating.get(0);
+        return nonrepeating.get(0);
     }
 
     //Return maximum occurring character in the input string
@@ -52,23 +47,17 @@ public class StringImp
             HashMap<Character, Integer> hash = new HashMap<Character, Integer>();
             char maxCh = 1;
             int maxCnt = 0;
-            for (char ch : txt.toCharArray())
-            {
-                if (hash.containsKey(ch))
-                {
+            for (char ch : txt.toCharArray()) {
+                if (hash.containsKey(ch)) {
                     int i = hash.get(ch);
                     hash.put(ch, i + 1);
-                    if (maxCnt < (i + 1))
-                    {
+                    if (maxCnt < (i + 1)) {
                         maxCh = ch;
                         maxCnt = 1 + i;
                     }
-                }
-                else
-                {
+                } else {
                     hash.put(ch, 1);
-                    if (maxCnt < 1)
-                    {
+                    if (maxCnt < 1) {
                         maxCh = ch;
                         maxCnt = 1;
                     }
@@ -83,21 +72,17 @@ public class StringImp
     the task was to find if that string could match another string with characters from: a-z
     where ' * ' can delete the character before it, and ' . ' could match whatever character.
     ' * ' always appear after a a-z character. */
-    Boolean isMatch(String sA, String sB, int iALength, int iBLength)
-    {
-        if(iALength==0&&iBLength==0)
+    Boolean isMatch(String sA, String sB, int iALength, int iBLength) {
+        if (iALength == 0 && iBLength == 0)
             return true;
-        else
-        {
-            if(iALength>0)
-            {
-                if(sA.charAt(iALength-1)=='*')
-                    return isMatch(sA,sB,iALength-1,iBLength)||isMatch(sA,sB,iALength-2,iBLength);
-                if(sA.charAt(iALength-1)=='.')
-                    return isMatch(sA,sB,iALength-1,iBLength)||isMatch(sA,sB,iALength-1,iBLength-1);
-                if(iBLength>0)
-                {
-                    if(sA.charAt(iALength-1)==sB.charAt(iBLength-1))
+        else {
+            if (iALength > 0) {
+                if (sA.charAt(iALength - 1) == '*')
+                    return isMatch(sA, sB, iALength - 1, iBLength) || isMatch(sA, sB, iALength - 2, iBLength);
+                if (sA.charAt(iALength - 1) == '.')
+                    return isMatch(sA, sB, iALength - 1, iBLength) || isMatch(sA, sB, iALength - 1, iBLength - 1);
+                if (iBLength > 0) {
+                    if (sA.charAt(iALength - 1) == sB.charAt(iBLength - 1))
                         return true;
                     else
                         return false;
@@ -132,32 +117,50 @@ public class StringImp
             }
         }
     }
+
     /* Given a string, find the length of the longest substring without repeating characters.
        For example, the longest substring without repeating letters for “abcabcbb” is “abc” */
     public int lengthOfLongestSubstring(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        int i=0;
-        int maxLength=0;
-        while(i<s.length())
-        {   int len=0;
-            Hashtable<Character,Integer> h= new Hashtable<Character,Integer>();
-            while(i<s.length() && h.get((Character) s.charAt(i))==null  )
-            {
-                h.put(s.charAt(i),i);
+        int i = 0;
+        int maxLength = 0;
+        while (i < s.length()) {
+            int len = 0;
+            Hashtable<Character, Integer> h = new Hashtable<Character, Integer>();
+            while (i < s.length() && h.get((Character) s.charAt(i)) == null) {
+                h.put(s.charAt(i), i);
                 i++;
                 len++;
-                maxLength=Math.max(maxLength,len);
+                maxLength = Math.max(maxLength, len);
             }
-            if(i==s.length())
-            {
+            if (i == s.length()) {
                 break;
-            }
-            else
-            {
-                i=h.get(s.charAt(i))+1;
+            } else {
+                i = h.get(s.charAt(i)) + 1;
             }
         }
         return maxLength;
     }
+
+    //Run of length: count the number of individual occurrences of repeated letters
+    public static int count_runs(String target) {
+        char prev = target.charAt(0);
+        int rpt = 0;
+        for (int i = 1; i < target.length(); i++) {
+            char curr = target.charAt(i);
+
+            if (curr == prev) {
+                rpt++;
+
+                while (i < target.length() - 1 && (target.charAt(i + 1) == curr || target.charAt(i + 1) == ' ' || target.charAt(i + 1) == '.')) {
+                    i++;
+                }
+            } else {
+                prev = curr;
+            }
+        }
+        return rpt;
+    }
 }
+
