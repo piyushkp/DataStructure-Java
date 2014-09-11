f112d3543c680996bbf46f44cb2fa35aa79287df
Piyush <er.piyushpatel@gmail.com>
2014-09-10 17:20:11 -0700

    Updated

--- src/code/ds/QueueImp.java
+++ src/code/ds/QueueImp.java
@@ -56,4 +56,35 @@ public class QueueImp
             return this.queue.remove(0);
         }
     }
+    //Implemented simple thread safe circular queue
+    public class CircularQueue {
+        private int size;
+        private int head;
+        private int tail;
+        private int q[];
+        public CircularQueue(int s) {
+            size = s;
+            q = new int[s+1];
+            head = 0;
+            tail = 0;
+        }
+        public synchronized void initialize() {
+            head = 0;
+            tail = 0;
+        }
+        public synchronized boolean enqueue(int v) {
+            int tmp = (tail+1) % size;
+            if (tmp == head) return false;
+            q[tail] = v;
+            tail = tmp;
+            return true;
+        }
+        public synchronized int dequeue() throws Exception{
+            if (head == tail) throw new Exception("queue underflow!");
+            int tmp = q[head];
+            head = (head + 1) % size;
+            return tmp;
+        }
+    }
+
 }
