e9426f3cad6f98d3408e3691a2465de3a0442590
Piyush <er.piyushpatel@gmail.com>
2014-09-15 10:35:29 -0700

    Updated

--- src/code/ds/Threading.java
+++ src/code/ds/Threading.java
@@ -33,5 +33,39 @@ public class Threading
             }
         }
     }
+    // Deadlock example in Java
+    private void deadLock() {
+        final Object resource1 = "resource1";
+        final Object resource2 = "resource2";
+        Thread t1 = new Thread()
+        {
+            public void run()
+            {
+                   synchronized (resource1)
+                   {
+                        System.out.println("Thread 1: locked resource 1");
+                        synchronized (resource2)
+                        {
+                            System.out.println("Thread 1: locked resource 2");
+                        }
+                   }
+            }
+        };
+        Thread t2 = new Thread() {
+            public void run()
+            {
+                synchronized (resource2)
+                {
+                    System.out.println("Thread 2: locked resource 2");
+                    synchronized (resource1)
+                    {
+                        System.out.println("Thread 2: locked resource 1");
+                    }
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+    }
     //Implement a delayed scheduler for jobs using pthread apis
 }
