3779f5370000bcb6ea91a546b063ca803fe6f183
Piyush <er.piyushpatel@gmail.com>
2014-09-15 16:19:48 -0700

    Updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -811,5 +811,28 @@ public class Tree {
         root.right = null;
         return newRoot;
     }
+    // Build max-heap
+    public static void BuildMaxHeap( int[ ] arr )
+    {
+        for( int i = arr.length - 1 ; i >= 0; i-- )
+            MaxHeapify( arr, i );
+    }
+    public static void MaxHeapify( int[ ] arr, int i )
+    {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if( left < arr.length && arr[ left ] > arr[ largest ] )
+            largest = left;
+        if( right < arr.length && arr[ right ] > arr[ largest ] )
+            largest = right;
+        if( largest != i )
+        {
+            int temp = arr[ i ];
+            arr[ i ] = arr[ largest ];
+            arr[ largest ] = temp;
+            MaxHeapify( arr, largest );
+        }
+    }
 }
 
