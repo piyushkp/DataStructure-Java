140d3225747b9d9a914dd6425bfcf09cc9901712
piyushkp <er.piyushpatel@gmail.com>
2014-09-20 13:34:02 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -160,7 +160,7 @@ public class Array
             int curr_max = a[0];
             for (int i = 1; i < a.length; i++)
             {
-                curr_max = Math.max(a[i], curr_max+a[i]);
+                curr_max = Math.max(a[i], curr_max + a[i]);
                 max_so_far = Math.max(max_so_far, curr_max);
             }
             return max_so_far;
