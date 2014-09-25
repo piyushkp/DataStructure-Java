7531e149a92f22771d21eb99ad0f6f1e87edbb07
Piyush <er.piyushpatel@gmail.com>
2014-09-25 16:07:19 -0700

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -405,7 +405,6 @@ public class Array {
         }
         return a[min];
     }
-
     //Find k closest elements to a given value
     //Input: K = 4, X = 35 arr[] = {12, 16, 22, 30, 35, 39, 42,45, 48, 50, 53, 55, 56}
     //Output: 30 39 42 45
