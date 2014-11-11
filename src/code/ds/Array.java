9eabe2b2315ec91e4399a4e1620148f6f28ff92a
Piyush <er.piyushpatel@gmail.com>
2014-11-11 09:58:51 -0800

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -628,4 +628,29 @@ public class Array {
             }
         }
     }
+    //Given two sorted arrays and a number x, find the pair whose sum is closest to x and the pair has an element from each array.
+    //ar1[] = {1, 4, 5, 7}; ar2[] = {10, 20, 30, 40};    x = 32     Output:  1 and 30
+    void printClosest(int ar1[], int ar2[], int m, int n, int x)
+    {
+        int diff = Integer.MAX_VALUE;
+        int res_l = 0, res_r = 0;
+        int l = 0, r = n-1;
+        while (l<m && r>=0)
+        {
+            // If this pair is closer to x than the previously
+            // found closest, then update res_l, res_r and diff
+            if (Math.abs(ar1[l] + ar2[r] - x) < diff)
+            {
+                res_l = l;
+                res_r = r;
+                diff = Math.abs(ar1[l] + ar2[r] - x);
+            }
+            // If sum of this pair is more than x, move to smaller side
+            if (ar1[l] + ar2[r] > x)
+                r--;
+            else  // move to the greater side
+                l++;
+        }
+        System.out.print(ar1[res_l] + " " + ar2[res_r]);
+    }
 }
