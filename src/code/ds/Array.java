537e3d07ea3b0fbc2f0f1cb6d8cdb9740716d653
Piyush <er.piyushpatel@gmail.com>
2014-12-23 11:35:51 -0800

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -714,5 +714,49 @@ public class Array {
             max_index = prev_zero;
         return max_index;
     }
-
+    //Given a set of n nuts of different sizes and n bolts of different sizes.
+    // There is a one-one mapping between nuts and bolts. Match nuts and bolts efficiently.
+    // Nuts and bolts are represented as array of characters
+    //char nuts[] = {'@', '#', '$', '%', '^', '&'};
+    //char bolts[] = {'$', '%', '&', '^', '@', '#'};
+    // Method which works just like quick sort
+    private static void matchPairs(char[] nuts, char[] bolts, int low, int high)
+    {
+        if (low < high)
+        {
+            // Choose last character of bolts array for nuts partition.
+            int pivot = partition(nuts, low, high, bolts[high]);
+            // Now using the partition of nuts choose that for bolts partition.
+            partition(bolts, low, high, nuts[pivot]);
+            // Recur for [low...pivot-1] & [pivot+1...high] for nuts and bolts array.
+            matchPairs(nuts, bolts, low, pivot-1);
+            matchPairs(nuts, bolts, pivot+1, high);
+        }
+    }
+    // Similar to standard partition method. Here we pass the pivot element
+    // too instead of choosing it inside the method.
+    private static int partition(char[] arr, int low, int high, char pivot)
+    {
+        int i = low;
+        char temp1, temp2;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot){
+                temp1 = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp1;
+                i++;
+            } else if(arr[j] == pivot){
+                temp1 = arr[j];
+                arr[j] = arr[high];
+                arr[high] = temp1;
+                j--;
+            }
+        }
+        temp2 = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp2;
+        // Return the partition index of an array based on the pivot element of other array.
+        return i;
+    }
 }
