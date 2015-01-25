02dc104a476110d050c9c87360f8d5682ff2d3fe
piyushkp <er.piyushpatel@gmail.com>
2015-01-24 21:00:04 -0800

    updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -792,6 +792,20 @@ public class Tree {
         }
         return succ;
     }
+    Node inOrderSuccessorWithParent(Node root, Node n)
+    {
+        // step 1 of the above algorithm
+        if( n.right != null )
+            return minValue(n.right);
+        // step 2 of the above algorithm
+       Node p = n.parent;
+        while(p != null && n == p.right)
+        {
+            n = p;
+            p = p.parent;
+        }
+        return p;
+    }
     private Node minValue(Node node) {
         Node curr = node;
         while (curr.left != null)
