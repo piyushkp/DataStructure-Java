29b7032df7db32749ed54463556080f37560f536
Piyush <er.piyushpatel@gmail.com>
2015-02-03 17:02:03 -0800

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -703,6 +703,59 @@ public class StringImp {
            }
        }
     }
+    //Find if a given string can be represented from a substring by iterating the substring “n” times
+    //Input: str = "abcabcabc"  Output: true The given string is 3 times repetition of "abc"
+    // Returns true if str is repetition of one of its sub Strings else return false.
+    Boolean isRepeat(char str[])
+    {
+        // Find length of string and create an array to
+        // store lps values used in KMP
+        int n = str.length;
+        int [] lps = new int[n];
+        // Pre-process the pattern (calculate lps[] array)
+        computeLPSArray(str, n, lps);
+        // Find length of longest suffix which is also prefix of str.
+        int len = lps[n-1];
+        // If there exist a suffix which is also prefix AND
+        // Length of the remaining substring divides total
+        // length, then str[0..n-len-1] is the substring that
+        // repeats n/(n-len) times (Readers can print substring
+        // and value of n/(n-len) for more clarity.
+        return (len > 0 && n%(n-len) == 0)? true: false;
+    }
+    // A utility function to fill lps[] or compute prefix funcrion
+    // used in KMP string matching algorithm.
+    void computeLPSArray(char str[], int M, int lps[])
+    {
+        int len = 0; //lenght of the previous longest prefix suffix
+        int i;
+        lps[0] = 0; //lps[0] is always 0
+        i = 1;
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M)
+        {
+            if (str[i] == str[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                if (len != 0)
+                {
+                    // This is tricky. Consider the example AAACAAAA and i = 7.
+                    len = lps[len-1];
+                    // Also, note that we do not increment i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
 
 }
 
