326f9aa2542df891f5ea5a14e627067ad7f918e4
Piyush <er.piyushpatel@gmail.com>
2016-03-11 16:19:23 -0800

    Updated

--- src/code/ds/Trie.java
+++ src/code/ds/Trie.java
@@ -62,4 +62,69 @@ public class Trie
             output(newlist,indent);
         }
     }
+    //Finding Shortest unique Prefixes for Strings in an Array
+    // Input = {"zebra", "dog", "duck", "dove"}     Output: dog, dov, du, z
+    //Time complexity if this step also is O(N) where N is total number of characters in all words.
+    class trieNode {
+        trieNode child[];
+        int freq;  // To store frequency
+    };
+
+    // Function to create a new trie node.
+    trieNode newTrieNode(){
+    trieNode newNode = new trieNode();
+    newNode.freq   = 1;
+    for (int i = 0; i<MAX; i++)
+        newNode->child[i] = NULL;
+    return newNode;
+}
+
+    // Method to insert a new string into Trie
+    void insert(trieNode root, String str){
+        int len = str.length();
+        trieNode pCrawl = root;
+        // Traversing over the length of given str.
+        for (int level = 0; level<len; level++){
+            // Get index of child node from current character in str.
+            int index = str.indexOf(level);
+            // Create a new child if not exist already
+            if (pCrawl.child[index] != null)
+                pCrawl.child[index] = newTrieNode();
+            else
+                (pCrawl.child[index].freq)++;
+            // Move to the child
+            pCrawl = pCrawl.child[index];
+        }
+    }
+
+    // This function prints unique prefix for every word stored in Trie. Prefixes one by one are stored in prefix[].
+    // 'ind' is current index of prefix[]
+    void findPrefixesUtil(trieNode root, char prefix[],int ind){
+        // Corner case
+        if (root == null)
+            return;
+        // Base case
+        if (root.freq == 1){
+            System.out.println(prefix);
+            return;
+        }
+        for (int i=0; i<256; i++){
+            if (root.child[i] != null){
+                prefix[ind] = i;
+                findPrefixesUtil(root.child[i], prefix, ind+1);
+            }
+        }
+    }
+    // Function to print all prefixes that uniquely represent all words in arr[0..n-1]
+    void findPrefixes(String arr[], int n) {
+        // Construct a Trie of all words
+        trieNode root = newTrieNode();
+        root.freq = 0;
+        for (int i = 0; i<n; i++)
+            insert(root, arr[i]);
+        // Create an array to store all prefixes. Maximum length of an input word is 500
+        char prefix [] = new char[500];
+        // Print all prefixes using Trie Traversal
+        findPrefixesUtil(root, prefix, 0);
+    }
 }
