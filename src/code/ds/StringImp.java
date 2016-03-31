da1d22801a3b69890e3a5f88605e71b0305c3dd4
piyushkp <er.piyushpatel@gmail.com>
2016-03-30 19:32:30 -0700

    updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -214,6 +214,46 @@ public class StringImp {
         return L[m][n];
     }
 
+    //Given a character limit and a message, split the message up into annotated chunks without cutting words as,
+    //for example when sending the SMS "Hi Sivasrinivas, your Uber is arriving now!" with char limit 25, you should get
+    //["Hi Sivasrinivas,(1/3)", "your Uber is arriving(2/3)", "now!(3/3)"]
+    static ArrayList<String> splitText(String message, int charLimit) {
+        return splitTextAuxUsingSplit(message, charLimit);
+    }
+    static ArrayList<String> splitTextAuxUsingSplit(String message, int charLimitOriginal) {
+        //Decrease the char limit to accomodate chunk number at the end i.e. (1/3). For now assuming, the message chunks won't be more than 9
+        int charLimit = charLimitOriginal - 5;
+        //New arraylist to store message chunks
+        ArrayList<String> result = new ArrayList<String>();
+        String[] splitted = message.split(" ");
+        String temp;
+        for (int i = 0; i < splitted.length - 1; i++) {
+            temp = splitted[i];
+            //while length of temp and the next element combined is less than charLimit, temp = temp + next element
+            //Last element to be taken care of after this loop
+            while (i + 1 < splitted.length - 1 && (temp + 1 + splitted[i + 1]).length() <= charLimit) {  //+1 for space
+                temp = temp + " " + splitted[i + 1];
+                i++;
+            }
+            result.add(temp);
+        }
+        //Take care of the last element
+        //Add the last element from splitted to the last element of result if their combined length is less than charLimit
+        String lastElement = result.get(result.size() - 1);
+        if (lastElement.length() + 1 + splitted[splitted.length - 1].length() < charLimit) {  //+1 for space
+            result.set(result.size() - 1, lastElement + " " + splitted[splitted.length - 1]);
+        } else {
+            result.add(splitted[splitted.length - 1]);
+        }
+        //append message chunk number for ex (1/3)
+        int resultSize = result.size();
+        for(int i = 0; i < resultSize; i++) {
+            result.set(i, result.get(i) +"("+ (i+1) + "/" + resultSize + ")" );
+        }
+        return result;
+    }
+
+
     /*Given a regular expression with characters a-z, ' * ', ' . '
     the task was to find if that string could match another string with characters from: a-z
     where ' * ' can delete the character before it, and ' . ' could match whatever character.
