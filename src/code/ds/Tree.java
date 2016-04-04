3da3fb30e7927655763d4385debcd427e997d838
Piyush <er.piyushpatel@gmail.com>
2016-04-04 10:59:06 -0700

    Updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -743,6 +743,24 @@ public class Tree {
         }
         return newNode;
     }
+    //Given a binary tree, check whether it is a mirror of itself.
+    boolean isMirror(Node node1, Node node2) {
+        // if both trees are empty, then they are mirror image
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        // For two trees to be mirror images, the following three conditions must be true
+        // 1 - Their root node's key must be same
+        // 2 - left subtree of left tree and right subtree of right tree have to be mirror images
+        // 3 - right subtree of left tree and left subtree of right tree have to be mirror images
+        if (node1 != null && node2 != null && node1.data == node2.data) {
+            return (isMirror(node1.left, node2.right)
+                    && isMirror(node1.right, node2.left));
+        }
+        // if neither of the above conditions is true then root1 and root2 are mirror images
+        return false;
+    }
+
     //Given the root of a binary search tree and 2 numbers min and max,
     //trim the tree such that all the numbers in the new tree are between min and max (inclusive).
     private Node trimBST(Node root, int minValue, int maxValue) {
