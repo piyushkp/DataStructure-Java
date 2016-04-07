ff7d40856bbcba9cf7ae6f7bdc0f3bfd9adc883f
Piyush <er.piyushpatel@gmail.com>
2016-04-07 16:37:07 -0700

    Updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -89,4 +89,26 @@ public class Matrix {
             }
         }
     }
+    //Given  an  image  represented  by  an  NxN matrix,  where  each  pixel  in  the  image  is  4 bytes, write a
+    //method to rotate the image by 90 degrees Can you do this in place? time: O(n), space: O(1)
+    public static void rotate(int[][] matrix, int n){
+        for(int layer = 0; layer < n/2; layer++){
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int i = first; i < last; i++){
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+                //left -> top
+                matrix[first][i] = matrix[last-offset][first];
+                //bottom -> left
+                matrix[last-offset][first]  = matrix[last][last - offset];
+                //right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+                //top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+    }
+
+
 }
