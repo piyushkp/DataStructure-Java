d1bae01072486d7c393ce414d6c0447e60d385b2
Piyush <er.piyushpatel@gmail.com>
2016-04-08 10:02:20 -0700

    Updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -100,4 +100,26 @@ public class Matrix {
             }
         }
     }
+    //Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0
+    public static void setZeros(int[][] matrix) {
+        int[] row = new int[matrix.length];
+        int[] column = new int[matrix[0].length];
+        // Store the row and column index with value 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    column[j] = 1;
+                }
+            }
+        }
+        // Set arr[i][j] to 0 if either row i or column j has a 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if ((row[i] == 1 || column[j] == 1)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
