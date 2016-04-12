fc737c21424666f3c960dd5df65b20f79bf68464
Piyush <er.piyushpatel@gmail.com>
2016-04-12 11:31:02 -0700

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -216,7 +216,7 @@ public class Array {
     int findlen(int[] a){
         int min = a[0];
         int max_len = 1;
-        int count = 0;
+        int count = 1;
         for (int i = 1; i < a.length; i++){
             if (a[i] > min){
                 count++;
