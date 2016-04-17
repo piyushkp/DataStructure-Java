a86f4149997f324f394a5f3db0f249e3143318a9
piyushkp <er.piyushpatel@gmail.com>
2016-04-16 22:55:13 -0700

    updated

--- src/code/ds/MISC.java
+++ src/code/ds/MISC.java
@@ -421,4 +421,25 @@ public class MISC
     private int distance(Point p1, Point p2){
         return (p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y);
     }
+    //draw a circle
+    void drawCircle(int r){
+        // Consider a rectangle of size N*N
+        int N = 2*r+1;
+        int x, y;  // Coordinates inside the rectangle
+        // Draw a square of size N*N.
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                // Start from the left most corner point
+                x = i-r;
+                y = j-r;
+                // If this point is inside the circle, print it
+                if (x*x + y*y <= r*r+1 )
+                    System.out.print(".");
+                else // If outside the circle, print space
+                    System.out.print(" ");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
