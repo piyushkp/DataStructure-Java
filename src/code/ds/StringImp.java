09bdfa0b47925d075e80ae1e6f08f1012b6202f3
Piyush <er.piyushpatel@gmail.com>
2016-04-18 14:58:48 -0700

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -156,6 +156,44 @@ public class StringImp {
         }
         return dp[m][n];
     }
+    //Given two string s1 and s2, find if s1 can be converted to s2 with exactly one edit.
+    //Time = O(m + n) space =O(1)
+    boolean isEditDistanceOne(String s1, String s2){
+        // Find lengths of given strings
+        int m = s1.length(), n = s2.length();
+        // If difference between lengths is more than 1, then strings can't be at one distance
+        if (Math.abs(m - n) > 1)
+            return false;
+        int count = 0; // Count of edits
+        int i = 0, j = 0;
+        while (i < m && j < n){
+            // If current characters don't match
+            if (s1.charAt(i) != s2.charAt(j)){
+                if (count == 1)
+                    return false;
+                // If length of one string is more, then only possible edit is to remove a character
+                if (m > n)
+                    i++;
+                else if (m< n)
+                    j++;
+                else {//If lengths of both strings is same
+                    i++;
+                    j++;
+                }
+                // Increment count of edits
+                count++;
+            }
+            else {// If current characters match
+                i++;
+                j++;
+            }
+        }
+        // If last character is extra in any string
+        if (i < m || j < n)
+            count++;
+        return count == 1;
+    }
+
     //Given an input string and a dictionary of words, find out if the input string can be segmented into a
     //space-separated sequence of dictionary words.
     //Consider the following dictionary { i, like, sam, sung, samsung, mobile, ice,cream, icecream, man, go, mango}
