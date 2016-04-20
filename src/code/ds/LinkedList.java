3eb803123321e81078b240c91ba6e8b9f71cdcfc
piyushkp <er.piyushpatel@gmail.com>
2016-04-19 21:21:42 -0700

    updated

--- src/code/ds/LinkedList.java
+++ src/code/ds/LinkedList.java
@@ -601,7 +601,6 @@ public class LinkedList {
             current.next = new_node;
         }
     }
-
     //Write a function to remove a single occurrence of an integer from a doubly linked list if it is present.
     void remove(Node head, int value) {
         Node cur = head;
@@ -615,6 +614,50 @@ public class LinkedList {
             }
             cur = cur.next;
         }
+    }
+    //Write a function to get the intersection point of two Linked Lists.
+    int getInterSectionNode(Node head1, Node head2) {
+        int c1 = getCount(head1);
+        int c2 = getCount(head2);
+        int d;
+        if (c1 > c2) {
+            d = c1 - c2;
+            return _getIntesectionNode(d, head1, head2);
+        } else {
+            d = c2 - c1;
+            return _getIntesectionNode(d, head2, head1);
+        }
+    }
+    /* function to get the intersection point of two linked lists head1 and head2 where head1 has d more nodes than head2 */
+    int _getIntesectionNode(int d, Node node1, Node node2) {
+        int i;
+        Node current1 = node1;
+        Node current2 = node2;
+        for (i = 0; i < d; i++) {
+            if (current1 == null) {
+                return -1;
+            }
+            current1 = current1.next;
+        }
+        while (current1 != null && current2 != null) {
+            if (current1.data == current2.data) {
+                return current1.data;
+            }
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+
+        return -1;
+    }
+    /*Takes head pointer of the linked list and returns the count of nodes in the list */
+    int getCount(Node node) {
+        Node current = node;
+        int count = 0;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
 
 }
