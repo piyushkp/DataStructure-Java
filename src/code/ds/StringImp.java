da44860f968935cd2c08a5c2b5504135b8001cd0
piyushkp <er.piyushpatel@gmail.com>
2016-04-23 20:14:26 -0700

    updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -1311,5 +1311,29 @@ public class StringImp {
         }
         return words;
     }
+    //Remove “b” and “ac” from a given string
+    // input = ababaac output = aaa input = abc output =""
+    static String Remove_Pattern_from_String(char[] str) {
+        int n = str.length;
+        int i = -1;  // previous character
+        int j = 0;   // current character
+        while (j < n) {
+            /* check if current and next character forms ac */
+            if (j < n - 1 && str[j] == 'a' && str[j + 1] == 'c')
+                j += 2;
+                /* if current character is b */
+            else if (str[j] == 'b')
+                j++;
+            /* if current char is 'c && last char in output is 'a' so delete both */
+            else if (i >= 0 && str[j] == 'c' && str[i] == 'a') {
+                i--;
+                j++;
+            }
+                /* else copy curr char to output string */
+            else
+                str[++i] = str[j++];
+        }
+        return new String(str).substring(0,++i);
+    }
 }
 
