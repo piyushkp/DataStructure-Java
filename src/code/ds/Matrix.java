6707ee00ed129e719e9194f2b8f117b0f5ef5d8d
Piyush <er.piyushpatel@gmail.com>
2016-04-25 15:24:16 -0700

    Updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -194,4 +194,36 @@ public class Matrix {
         Coordinate dest = new Coordinate(matrix.length - 1, matrix[0].length - 1);
         return findElement(matrix, origin, dest, x);
     }
+    /*Given a 2D matrix(square or rectangular) print it in spiral way.
+        e.g 1 2 3
+            4 5 6
+            7 8 9
+     Printing should be 1 2 3 6 9 8 7 4 5 */
+    public void print(int matrix[][]) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int up = 0;
+        int down = matrix.length - 1;
+        while (left < right && up < down) {
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[up][i] + " ");
+            }
+            up++;
+            for (int i = up; i <= down; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                System.out.print(matrix[down][i] + " ");
+            }
+            down--;
+            for (int i = down; i >= up; i--) {
+                System.out.print(matrix[i][left] + " ");
+            }
+            left++;
+        }
+    }
 }
