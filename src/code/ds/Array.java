10e15edf207a759c1e8ad328d9d1e2360ebf2ee7
piyushkp <er.piyushpatel@gmail.com>
2016-04-26 17:46:08 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -1701,5 +1701,46 @@ public class Array {
         }
         return bytes;
     }
+    //Given an unsorted array of numbers, write a function that returns true if array consists of consecutive numbers.
+    //http://www.geeksforgeeks.org/check-if-array-elements-are-consecutive/
+    boolean areConsecutive(int arr[], int n){
+        if ( n <  1 )
+            return false;
+  /* 1) Get the minimum element in array */
+        int min = getMin(arr, n);
+  /* 2) Get the maximum element in array */
+        int max = getMax(arr, n);
+  /* 3) max - min + 1 is equal to n,  then only check all elements */
+        if (max - min  + 1 == n){
+      /* Create a temp array to hold visited flag of all elements. Note that, calloc is used here so that all values are initialized
+         as false */
+            boolean[] visited = new boolean[n];
+            int i;
+            for (i = 0; i < n; i++){
+         /* If we see an element again, then return false */
+                if ( visited[arr[i] - min] != false )
+                    return false;
+         /* If visited first time, then mark the element as visited */
+                visited[arr[i] - min] = true;
+            }
+      /* If all elements occur once, then return true */
+            return true;
+        }
+        return false; // if (max - min  + 1 != n)
+    }
+    int getMin(int arr[], int n){
+        int min = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] < min)
+                min = arr[i];
+        return min;
+    }
+    int getMax(int arr[], int n){
+        int max = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > max)
+                max = arr[i];
+        return max;
+    }
 
 }
