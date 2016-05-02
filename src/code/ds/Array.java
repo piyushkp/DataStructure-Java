c89b1bd05c0de595a9fc2c8a904bf10a2fdad5aa
Piyush <er.piyushpatel@gmail.com>
2016-05-02 14:18:39 -0700

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -1877,5 +1877,46 @@ public class Array {
     //2) One by one remove min element from heap, put it in result array, and add a new element to heap from remaining elements.
     //So overall complexity will be O(k) + O((n-k)*logK)
 
+    /* Given a total and coins of certain denomination with infinite supply, what is the minimum number
+    * of coins it takes to form this total.
+    * Time complexity - O(coins.size * total)
+    * Space complexity - O(coins.size * total) */
+    public int minimumCoinBottomUp(int total, int coins[]){
+        int T[] = new int[total + 1];
+        int R[] = new int[total + 1];
+        T[0] = 0;
+        for(int i=1; i <= total; i++){
+            T[i] = Integer.MAX_VALUE-1;
+            R[i] = -1;
+        }
+        for(int j=0; j < coins.length; j++){
+            for(int i=1; i <= total; i++){
+                if(i >= coins[j]){
+                    if (T[i - coins[j]] + 1 < T[i]) {
+                        T[i] = 1 + T[i - coins[j]];
+                        R[i] = j;
+                    }
+                }
+            }
+        }
+        printCoinCombination(R, coins);
+        return T[total];
+    }
+    private void printCoinCombination(int R[], int coins[]) {
+        if (R[R.length - 1] == -1) {
+            System.out.print("No solution is possible");
+            return;
+        }
+        int start = R.length - 1;
+        System.out.print("Coins used to form total ");
+        while ( start != 0 ) {
+            int j = R[start];
+            System.out.print(coins[j] + " ");
+            start = start - coins[j];
+        }
+        System.out.print("\n");
+    }
+
+
 
 }
