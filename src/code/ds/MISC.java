65abffb4cbd169af910f85784b4b43c81e4036cc
Piyush <er.piyushpatel@gmail.com>
2016-05-04 10:58:15 -0700

    Updated

--- src/code/ds/MISC.java
+++ src/code/ds/MISC.java
@@ -684,4 +684,14 @@ public class MISC {
             }
         }
     }
+    //Given two rectangles, find if the given two rectangles overlap or not.
+    boolean doOverlap(Point l1, Point r1, Point l2, Point r2){
+        // If one rectangle is on left side of other
+        if (l1.x > r2.x || l2.x > r1.x)
+            return false;
+        // If one rectangle is above other
+        if (l1.y < r2.y || l2.y < r1.y)
+            return false;
+        return true;
+    }
 }
