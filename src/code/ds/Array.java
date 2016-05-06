104edd5b843317b9c3054603d7ee7e2ff1cb73ad
piyushkp <er.piyushpatel@gmail.com>
2016-05-05 22:32:12 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -1990,6 +1990,21 @@ public class Array {
         }
         System.out.println();
     }
+    //Maximum difference between two elements such that larger element appears after the smaller number
+    //Input =  [2, 3, 10, 6, 4, 8, 1]  output = 8
+    int maxDiff(int arr[], int arr_size){
+        int max_diff = arr[1] - arr[0];
+        int min_element = arr[0];
+        int i;
+        for(i = 1; i < arr_size; i++) {
+            if (arr[i] - min_element > max_diff)
+                max_diff = arr[i] - min_element;
+            if (arr[i] < min_element)
+                min_element = arr[i];
+        }
+        return max_diff;
+    }
+
 
 
 }
