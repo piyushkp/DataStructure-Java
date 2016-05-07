f945f5b3f2c04e57c6cb6e06ca47ed8a76ee2e78
piyushkp <er.piyushpatel@gmail.com>
2016-05-06 23:53:22 -0700

    updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -226,4 +226,51 @@ public class Matrix {
             left++;
         }
     }
+    /*Given a 2D grid of characters and a word, find all occurrences of given word in grid. A word can be matched in
+    all 8 directions at any point. Word is said be found in a direction if all characters match in this direction.
+    Input = grid[][] = {"GEEKSFORGEEKS",
+                        "GEEKSQUIZGEEK",
+                        "IDEQAPRACTICE"};
+        word = "GEEKS"
+    Output: pattern found at 0, 0
+            pattern found at 0, 8
+            pattern found at 1, 0      */
+    void patternSearch(char grid[][], String word){
+        // Consider every point as starting point and search given word
+        int R = grid.length; int C = grid[0].length;
+        for (int row = 0; row < R; row++)
+            for (int col = 0; col < C; col++)
+                if (search2D(grid, row, col, word))
+                    System.out.print("pattern found at "+ row +", " +col);
+    }
+    // For searching in all 8 direction
+    int x[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    int y[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+    boolean search2D(char grid[][], int row, int col, String word){
+        int R = grid.length; int C = grid[0].length;
+        // If first character of word doesn't match with given starting point in grid.
+        if (grid[row][col] != word.charAt(0))
+            return false;
+        int len = word.length();
+        // Search word in all 8 directions starting from (row,col)
+        for (int dir = 0; dir < 8; dir++){
+            // Initialize starting point for current direction
+            int k, rd = row + x[dir], cd = col + y[dir];
+            // First character is already checked, match remaining characters
+            for (k = 1; k < len; k++){
+                // If out of bound break
+                if (rd >= R || rd < 0 || cd >= C || cd < 0)
+                    break;
+                // If not matched,  break
+                if (grid[rd][cd] != word.charAt(k))
+                    break;
+                //  Moving in particular direction
+                rd += x[dir]; cd += y[dir];
+            }
+            // If all character matched, then value of must be equal to length of word
+            if (k == len)
+                return true;
+        }
+        return false;
+    }
 }
