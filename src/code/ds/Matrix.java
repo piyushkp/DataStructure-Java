30115117220c70b718773d8bdaa4dd927b9dadc1
piyushkp <er.piyushpatel@gmail.com>
2016-05-07 23:04:40 -0700

    updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -344,4 +344,70 @@ public class Matrix {
         }
         return maxArea;
     }
-}
+    /*Write a program to find maximum sum rectangle in give 2D matrix. Assume there is at least one positive number in the 2D matrix.
+     Solution: * Keep temp array with size as number of rows. Start left and right from 0
+     * and keep adding values for each row and maintain them in this temp array.
+     * Run Kadane's algorithm to find max sum subarray in temp. Now increment right by
+     * 1. When right reaches last column reset right to 1 and left to 1.
+     *Space complexity of this algorithm is O(row)
+     * Time complexity of this algorithm is O(row*col*col) */
+    public void maxSum(int input[][]){
+        int maxSum=0; int leftBound=0; int rightBound=0; int upBound=0; int lowBound=0;
+        int rows = input.length;
+        int cols = input[0].length;
+        int temp[] = new int[rows];
+        for(int left = 0; left < cols ; left++){
+            for(int i=0; i < rows; i++){
+                temp[i] = 0;
+            }
+            for(int right = left; right < cols; right++){
+                for(int i=0; i < rows; i++){
+                    temp[i] += input[i][right];
+                }
+                KadaneResult kadaneResult = kadane(temp);
+                if(kadaneResult.maxSum > maxSum){
+                    maxSum = kadaneResult.maxSum;
+                    leftBound = left;
+                    rightBound = right;
+                    upBound = kadaneResult.start;
+                    lowBound = kadaneResult.end;
+                }
+            }
+        }
+        System.out.print("Result [maxSum=" + maxSum + ", leftBound=" + leftBound
+                + ", rightBound=" + rightBound + ", upBound=" + upBound
+                + ", lowBound=" + lowBound + "]");
+    }
+    class KadaneResult{
+        int maxSum;
+        int start;
+        int end;
+        public KadaneResult(int maxSum, int start, int end) {
+            this.maxSum = maxSum;
+            this.start = start;
+            this.end = end;
+        }
+    }
+    //max sum subarray
+    private KadaneResult kadane(int arr[]){
+        int max = 0;
+        int maxStart = -1;
+        int maxEnd = -1;
+        int currentStart = 0;
+        int maxSoFar = 0;
+        for(int i=0; i < arr.length; i++){
+            maxSoFar += arr[i];
+            if(maxSoFar < 0){
+                maxSoFar = 0;
+                currentStart = i+1;
+            }
+            if(max < maxSoFar){
+                maxStart = currentStart;
+                maxEnd = i;
+                max = maxSoFar;
+            }
+        }
+        return new KadaneResult(max, maxStart, maxEnd);
+    }
+
+ }
