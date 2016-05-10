60dcbd7ec221c564b3dbe78634f59e56cbc9655c
piyushkp <er.piyushpatel@gmail.com>
2016-05-09 22:03:10 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -332,8 +332,8 @@ public class Array {
     }
     //Given an unsorted array of non-negative integers, find a continous subarray which adds to a given number.
     //arr[] = {1, 4, 20, 3, 10, 5}, sum = 33  output = true
-    //Time complexity - O(input.size * total_sum)
-    //Space complexity - O(input.size*total_sum)
+    //Time complexity - O(input.size * total_sum) Space complexity - O(input.size*total_sum)
+    //https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/SubsetSum.java
     public boolean subsetSum(int input[], int total) {
         boolean T[][] = new boolean[input.length + 1][total + 1];
         for (int i = 0; i <= input.length; i++) {
