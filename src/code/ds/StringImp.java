0b859cdcde96b0787d0251cf432865b57ec3dd8d
piyushkp <er.piyushpatel@gmail.com>
2016-05-17 22:18:45 -0700

    updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -1906,5 +1906,22 @@ public class StringImp {
         }
         return false;
     }
+    //tiny URL
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int    BASE     = ALPHABET.length();
+    public static String encode(int num) {
+        StringBuilder sb = new StringBuilder();
+        while ( num > 0 ) {
+            sb.append( ALPHABET.charAt( num % BASE ) );
+            num /= BASE;
+        }
+        return sb.reverse().toString();
+    }
+    public static int decode1(String str) {
+        int num = 0;
+        for ( int i = 0; i < str.length(); i++ )
+            num = num * BASE + ALPHABET.indexOf(str.charAt(i));
+        return num;
+    }
 }
 
