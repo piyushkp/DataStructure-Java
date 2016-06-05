f798d2a1a47f0136fc54fa2d49bfcbcf3f6557b9
piyushkp <er.piyushpatel@gmail.com>
2016-06-04 22:54:25 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -309,23 +309,28 @@ public class Array {
     }
     //Given an unsorted array of non-negative integers, find a continous subarray which adds to a given number.
     //arr[] = {1, 4, 20, 3, 10, 5}, sum = 33  output = true
-    //Time complexity - O(input.size * total_sum) Space complexity - O(input.size*total_sum)
-    //https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/SubsetSum.java
-    public boolean subsetSum(int input[], int total) {
-        boolean T[][] = new boolean[input.length + 1][total + 1];
-        for (int i = 0; i <= input.length; i++) {
-            T[i][0] = true;
-        }
-        for (int i = 1; i <= input.length; i++) {
-            for (int j = 1; j <= total; j++) {
-                if (j - input[i - 1] >= 0) {
-                    T[i][j] = T[i - 1][j] || T[i - 1][j - input[i - 1]];
-                } else {
-                    T[i][j] = T[i-1][j];
-                }
+    int subArraySum(int arr[], int n, int sum){
+    /* Initialize curr_sum as value of first element and starting point as 0 */
+        int curr_sum = arr[0], start = 0, i;
+    /* Add elements one by one to curr_sum and if the curr_sum exceeds the sum, then remove starting element */
+        for (i = 1; i <= n; i++){
+            // If curr_sum exceeds the sum, then remove the starting elements
+            while (curr_sum > sum && start < i-1){
+                curr_sum = curr_sum - arr[start];
+                start++;
             }
+            // If curr_sum becomes equal to sum, then return true
+            if (curr_sum == sum){
+                System.out.println ("Sum found between indexes "+start+" and "+ (i-1));
+                return 1;
+            }
+            // Add this element to curr_sum
+            if (i < n)
+                curr_sum = curr_sum + arr[i];
         }
-        return T[input.length][total];
+        // If we reach here, then no subarray
+        System.out.println("No subarray found");
+        return 0;
     }
     //find the sum of contiguous sub array within a one-dimensional array of numbers with negative which has the largest sum .
     // input {-2, -3, 4, -1, -2, 1, 5, -3} output = 7
