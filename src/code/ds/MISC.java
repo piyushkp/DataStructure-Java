c4d038b7997ac0ed4f8a91d829fe5b685c376018
Piyush <er.piyushpatel@gmail.com>
2016-06-16 17:10:01 -0700

    Updated

--- src/code/ds/MISC.java
+++ src/code/ds/MISC.java
@@ -902,4 +902,31 @@ public class MISC {
             map.put(key,node);
         }
     }
+    //Implement a peek using a existing iterator next and hasnext function.
+    class PeekingIterator implements Iterator<Integer> {
+        private Integer next; //cache the next peek
+        private Iterator<Integer> iter;
+        public PeekingIterator(Iterator<Integer> iterator) {
+            // initialize any member here.
+            iter = iterator;
+            if (iter.hasNext()) {
+                next = iter.next();
+            }
+        }
+        // Returns the next element in the iteration without advancing the iterator.
+        public Integer peek() {
+            return next;
+        }
+        // hasNext() and next() should behave the same as in the Iterator interface. Override them if needed.
+        @Override
+        public Integer next() {
+            Integer ret = next;
+            next = iter.hasNext() ? iter.next() : null;
+            return ret;
+        }
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+    }
 }
