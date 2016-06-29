7cd636706fd75802e70e716d3782a5679ca19031
piyushkp <er.piyushpatel@gmail.com>
2016-06-29 08:57:36 -0700

    updated

--- src/code/ds/Matrix.java
+++ src/code/ds/Matrix.java
@@ -37,12 +37,22 @@ public class Matrix {
     }
     //Given a grid of size m by n, write an algorithm that
     //computes all paths from 0,0 to m,n such that you can always step horizontally or vertically but cannot reverse.
-    int FindAllPaths(int x, int y, int Xmax, int Ymax) {
-        int nResult = 0;
-        if (x == Xmax && y == Ymax) return 1;
-        if (x < Xmax) nResult += FindAllPaths(x + 1, y, Xmax, Ymax);
-        if (y < Ymax) nResult += FindAllPaths(x, y + 1, Xmax, Ymax);
-        return nResult;
+    int numberOfPaths(int m, int n){
+        // Create a 2D table to store results of subproblems
+        int count[][]  = new int[m][n];
+        // Count of paths to reach any cell in first column is 1
+        for (int i = 0; i < m; i++)
+            count[i][0] = 1;
+        // Count of paths to reach any cell in first column is 1
+        for (int j = 0; j < n; j++)
+            count[0][j] = 1;
+        // Calculate count of paths for other cells in bottom-up manner using the recursive solution
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++)
+                // By uncommenting the last part the code calculatest he total possible paths if the diagonal Movements are allowed
+                count[i][j] = count[i-1][j] + count[i][j-1]; //+ count[i-1][j-1];
+        }
+        return count[m-1][n-1];
     }
     //Given  an  image  represented  by  an  NxN matrix,  where  each  pixel  in  the  image  is  4 bytes, write a
     //method to rotate the image by 90 degrees Can you do this in place? time: O(n), space: O(1)
@@ -436,6 +446,54 @@ public class Matrix {
                 OceanUtil(M, row + rowNbr[k], col + colNbr[k], visited);
             }
     }
+    /*A Maze is given as N*N binary matrix of blocks where source block is the upper left most block and destination
+    block is lower rightmost block i.e., maze[N-1][N-1]. A rat starts from source and has to reach destination.
+    The rat can move only in two directions: forward and down.*/
+    /* A utility function to check if x,y is valid index for N*N maze */
+    static final int M = 4, N = 4;
+    boolean isSafe(int maze[][], int x, int y){
+        // if (x,y outside maze) return false
+        return (x >= 0 && x < M && y >= 0 && y < N && maze[x][y] == 1);
+    }
+    /* This function solves the Maze problem using Backtracking. It mainly uses solveMazeUtil() to solve the problem. It returns false if no
+       path is possible, otherwise return true and prints the path in the form of 1s. Please note that there may be more than one solutions, this
+       function prints one of the feasible solutions.*/
+    boolean solveMaze(int maze[][]){
+        int sol[][] =   {   {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0}
+                        };
 
+        if (solveMazeUtil(maze, 0, 0, sol) == false){
+            System.out.print("Solution doesn't exist");
+            return false;
+        }
+        //printSolution(sol);
+        return true;
+    }
+    /* A recursive utility function to solve Maze problem */
+    boolean solveMazeUtil(int maze[][], int x, int y, int sol[][]){
+        // if (x,y is goal) return true
+        if (x == M - 1 && y == N - 1){
+            sol[x][y] = 1;
+            return true;
+        }
+        // Check if maze[x][y] is valid
+        if (isSafe(maze, x, y) == true){
+            // mark x,y as part of solution path
+            sol[x][y] = 1;
+            /* Move forward in x direction */
+            if (solveMazeUtil(maze, x + 1, y, sol))
+                return true;
+             /* If moving in x direction doesn't give solution then  Move down in y direction */
+            if (solveMazeUtil(maze, x, y + 1, sol))
+                return true;
+             /* If none of the above movements work then BACKTRACK: unmark x,y as part of solution path */
+            sol[x][y] = 0;
+            return false;
+        }
+        return false;
+    }
 
 }
