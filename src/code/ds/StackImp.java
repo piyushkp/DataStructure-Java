73f3c43103300f2521e8a7a4c6a7e59d063119de
piyushkp <er.piyushpatel@gmail.com>
2016-07-21 19:17:36 -0700

    updated

--- src/code/ds/StackImp.java
+++ src/code/ds/StackImp.java
@@ -18,19 +18,16 @@ public class StackImp {
         public StackArray() {
             arr = (T[]) new Object[2];
         }
-
         private void resize(int capacity) {
             T[] tmp = (T[]) new Object[capacity];
             System.arraycopy(arr, 0, tmp, 0, total);
             arr = tmp;
         }
-
         public StackArray<T> push(T ele) {
             if (arr.length == total) resize(arr.length * 2);
             arr[total++] = ele;
             return this;
         }
-
         public T pop() {
             if (total == 0) throw new java.util.NoSuchElementException();
             T ele = arr[--total];
@@ -38,7 +35,6 @@ public class StackImp {
             if (total > 0 && total == arr.length / 4) resize(arr.length / 2);
             return ele;
         }
-
         @Override
         public String toString() {
             return java.util.Arrays.toString(arr);
