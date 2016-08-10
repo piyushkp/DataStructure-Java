ee9dbb51633a04a12c8f802409686299d4876d7d
piyushkp <er.piyushpatel@gmail.com>
2016-08-10 14:31:36 -0700

    updated

--- src/code/ds/LinkList.java
+++ src/code/ds/LinkList.java
@@ -219,7 +219,7 @@ public class LinkList {
     }
 
     // Reverse Linked List
-    void Reverse() {
+    public static void Reverse(Node head) {
         // Initialize currentNode pointer to the start of the list and prevNode to null
         // (as the new list is currently pointing to null).
         Node currentNode = head;
@@ -395,6 +395,54 @@ public class LinkList {
         }
         // Append the even list at the end of odd list
         odd.next = even;
+    }
+    //Given a singly linked list L0 -> L1 -> … -> Ln-1 -> Ln.
+    //Rearrange the nodes in the list so that the new formed list is : L0 -> Ln -> L1 -> Ln-1 -> L2 -> Ln-2 …
+    //Input:  1 -> 2 -> 3 -> 4     Output: 1 -> 4 -> 2 -> 3
+    public void rearrange1(Node node) {
+        // 1) Find the middle point using tortoise and hare method
+        Node slow = node, fast = slow.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 2) Split the linked list in two halves, node1= head of first half    1 -> 2 -> 3 // node2, head of second half   4 -> 5
+        Node node1 = node;
+        Node node2 = slow.next;
+        slow.next = null;
+        // 3) Reverse the second half, i.e., 5 -> 4
+        node2 = reverselist(node2);
+        // 4) Merge alternate nodes
+        node = new Node(); // Assign dummy Node
+        // curr is the pointer to this dummy Node, which will be used to form the new list
+        Node curr = node;
+        while (node1 != null || node2 != null) {
+            // First add the element from first list
+            if (node1 != null) {
+                curr.next = node1;
+                curr = curr.next;
+                node1 = node1.next;
+            }
+            // Then add the element from second list
+            if (node2 != null) {
+                curr.next = node2;
+                curr = curr.next;
+                node2 = node2.next;
+            }
+        }
+        // Assign the head of the new list to head pointer
+        node = node.next;
+    }
+    public static Node reverselist(Node node) {
+        Node prev = null, curr = node, next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        node = prev;
+        return node;
     }
 
     //Given a linked list and two integers M and N. Traverse the linked list such that you retain M nodes then delete next N nodes,
