0e95c5168014b920e240f1504d49eefde9c3ce70
piyushkp <er.piyushpatel@gmail.com>
2016-08-17 19:28:28 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -260,14 +260,14 @@ public class Array {
     }
     //Given an array of integers, find two numbers such that they add up to a specific target number. 2-sum
     //Time = O(n) space O(n)
-    public int[] twoSum(int[] numbers, int target) {
+    public ArrayList<Integer> twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int[] result = new int[2];
+        ArrayList<Integer> result = new ArrayList<>();
         for (int i = 0; i < numbers.length; i++) {
             if (map.containsKey(numbers[i])) {
                 int index = map.get(numbers[i]);
-                result[0] = index+1 ;
-                result[1] = i+1;
+                result.add(index+1) ;
+                result.add(i+1);
                 break;
             } else {
                 map.put(target - numbers[i], i);
