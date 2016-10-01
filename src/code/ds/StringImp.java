bead1bcc54eb75da4972d097a8ee77f9144a03ed
piyushkp <er.piyushpatel@gmail.com>
2016-10-01 16:10:10 -0700

    updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -1455,6 +1455,35 @@ public class StringImp {
     /* Implement regex regular expression matching with support for '.' and '*'.
     '.' Matches any single character.
     '*' Matches zero or more of the preceding element. */
+    /**
+     * Dynamic programming technique for regex matching. Time and space = O(M * N)
+     */
+    public boolean matchRegex(char[] text, char[] pattern) {
+        boolean T[][] = new boolean[text.length + 1][pattern.length + 1];
+        T[0][0] = true;
+        //Deals with patterns like a* or a*b* or a*b*c*
+        for (int i = 1; i < T[0].length; i++) {
+            if (pattern[i-1] == '*') {
+                T[0][i] = T[0][i - 2];
+            }
+        }
+        for (int i = 1; i < T.length; i++) {
+            for (int j = 1; j < T[0].length; j++) {
+                if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
+                    T[i][j] = T[i-1][j-1];
+                } else if (pattern[j - 1] == '*')  {
+                    T[i][j] = T[i][j - 2];
+                    if (pattern[j-2] == '.' || pattern[j - 2] == text[i - 1]) {
+                        T[i][j] = T[i][j] | T[i - 1][j];
+                    }
+                } else {
+                    T[i][j] = false;
+                }
+            }
+        }
+        return T[text.length][pattern.length];
+    }
+    //non-dynamic exponential complexity
     public static boolean isMatch(String s, String p) {
         // base case
         if (p.length() == 0) {
