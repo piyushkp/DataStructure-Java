45a3d7effed9ec1387be6f65b7f2d510b0d13013
piyushkp <er.piyushpatel@gmail.com>
2016-10-21 14:55:49 -0700

    updated

--- src/code/ds/MISC.java
+++ src/code/ds/MISC.java
@@ -1121,7 +1121,7 @@ public class MISC {
             size = 0;
         }
     }
-    //Add a third dimension of time to a hashmap , so ur hashmap will look something like this - HashMap<K, t, V> where
+    //Add a third dimension of time to a hashmap,so ur hashmap will look something like this - HashMap<K, t, V> where
     //t is a float value. Implement the get and put methods to this map. The get method should be something like - map.get(K,t)
     //which should give us the value. If t does not exists then map should return the closest t' such that t' is smaller than t.
     //For example, if map contains (K,1,V1) and (K,2,V2) and the user does a get(k,1.5) then the output should be v1 as 1 is the next smallest number to 1.5
