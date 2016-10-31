e0028a0a0f727c8857088b2b7a618b5eb52ad397
piyushkp <er.piyushpatel@gmail.com>
2016-10-31 16:06:54 -0700

    updated

--- src/code/ds/Hasher.java
+++ src/code/ds/Hasher.java
@@ -150,3 +150,41 @@ class MiniCassandra {
         return results;
     }
 }
+class ConsistentHash<T> {
+    class HashFunction{
+        public int hash(Object key){
+            return 0;
+        }
+    }
+    private final HashFunction hashFunction;
+    private final int numberOfReplicas;
+    private final SortedMap<Integer, T> circle = new TreeMap<>();
+    public ConsistentHash(HashFunction hashFunction, int numberOfReplicas, Collection<T> nodes) {
+        this.hashFunction = hashFunction;
+        this.numberOfReplicas = numberOfReplicas;
+        for (T node : nodes) {
+            add(node);
+        }
+    }
+    public void add(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.put(hashFunction.hash(node.toString() + i), node);
+        }
+    }
+    public void remove(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.remove(hashFunction.hash(node.toString() + i));
+        }
+    }
+    public T get(Object key) {
+        if (circle.isEmpty()) {
+            return null;
+        }
+        int hash = hashFunction.hash(key);
+        if (!circle.containsKey(hash)) {
+            SortedMap<Integer, T> tailMap = circle.tailMap(hash);
+            hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
+        }
+        return circle.get(hash);
+    }
+}
