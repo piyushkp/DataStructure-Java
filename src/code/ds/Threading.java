ca307a58f54de4f9b55c0d65ff591a11e0122122
piyushkp <er.piyushpatel@gmail.com>
2016-11-26 20:35:51 -0800

    Updated

--- src/code/ds/Threading.java
+++ src/code/ds/Threading.java
@@ -2,7 +2,7 @@ package code.ds;
 /**
  * Created by Piyush Patel.
  */
-import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
 public class Threading {
     public static void main(String[] args) {
         System.out.print("Threading");
@@ -90,6 +90,57 @@ public class Threading {
                 IntPair newv = new IntPair(oldv.lower, i);
                 if (values.compareAndSet(oldv, newv)) return;
             }
+        }
+    }
+    //There are three threads in a process.The first thread prints 1 1 1 …, the second one prints 2 2 2 …, and the third one prints 3 3 3 … endlessly.
+    //How do you schedule these three threads in order to print 1 2 3 1 2 3 …?
+    class ThreeThreads {
+        public  void main(String[] args) {
+            final Lock lock = new ReentrantLock();
+            final Condition condition = lock.newCondition();
+
+            ThreadId threadId = new ThreeThreads.ThreadId();
+            threadId.setId(1);
+            Thread t1 = setThread(lock, condition, 1, 2, threadId);
+            Thread t2 = setThread(lock, condition, 2, 3, threadId);
+            Thread t3 = setThread(lock, condition, 3, 1, threadId);
+            t1.start();
+            t2.start();
+            t3.start();
+        }
+        private class ThreadId {
+            private int id;
+            public ThreadId(){}
+            public int getId() {
+                return id;
+            }
+            public void setId(int id) {
+                this.id = id;
+            }
+        }
+        private Thread setThread(final Lock lock, final Condition condition, int currentThreadId, int nextThreadId, ThreadId threadId) {
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    while (true) {
+                        lock.lock();
+                        try {
+                            while (threadId.getId() != currentThreadId)
+                                try {
+                                    condition.await();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            System.out.println("" + currentThreadId);
+                            threadId.setId(nextThreadId);
+                            condition.signalAll();
+                        } finally {
+                            lock.unlock();
+                        }
+                    }
+                }
+            };
+            return thread;
         }
     }
 }
