26b92328c6dc177e50753a7d78934647ae31aec9
piyushkp <er.piyushpatel@gmail.com>
2017-01-21 14:22:53 -0800

    Updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -1698,6 +1698,28 @@ public class Tree {
         System.out.println();
     }
 
+    // Print longest path from root to leaf in Binary tree
+    // Do BFS and maintain map to add parent node for all traverse node.
+    private void printLongestPath(Node root) {
+        Queue<Node> _queue = null;
+        HashMap<Node, Node> map = new HashMap<>();
+        map.put(root, null);
+        Node tmp = null;
+        _queue.add(root);
+        while (_queue.size() > 0) {
+            tmp = _queue.remove();
+            if (tmp.left != null) {
+                map.put(tmp.left, tmp);
+                _queue.add(tmp.left);
+            }
+            if (tmp.right != null) {
+                _queue.add(tmp.right);
+                map.put(tmp.right, tmp);
+            }
+        }
+        printTopToBottomPath(tmp, map);
+    }
+
     //Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a number x
 // (the number of values less than or equal to x). Implement the data structures and algorithms to support these
 // operations. That is, implement the method track(int x), which is called when each number is generated, and the
