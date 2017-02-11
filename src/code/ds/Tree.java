235d1fcbc55ab06d71a6a1c9fef61a5d0baf0d2d
piyushkp <er.piyushpatel@gmail.com>
2017-02-11 15:45:13 -0800

    Updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -902,6 +902,38 @@ public class Tree {
         a = b;
         b = t;
     }
+    /*  You are given a binary tree in which each node contains an integer value (which
+        might be positive or negative). Design an algorithm to count the number of paths that sum to a
+        given value. The path does not need to start or end at the root or a leaf, but it must go downwards
+        (traveling only from parent nodes to ch ild nodes). Time = O(N) space O(logN) or O(N) for unbalanced tree*/
+    public static int countPathsWithSum(Node node, int targetSum, int runningSum, HashMap<Integer, Integer> pathCount) {
+        if (node == null) return 0; // Base case
+        runningSum += node.data;
+		/* Count paths with sum ending at the current node. */
+        int sum = runningSum - targetSum;
+        int totalPaths = pathCount.getOrDefault(sum, 0);
+
+		/* If runningSum equals targetSum, then one additional path starts at root. Add in this path.*/
+        if (runningSum == targetSum) {
+            totalPaths++;
+        }
+		/* Add runningSum to pathCounts. */
+        incrementHashTable(pathCount, runningSum, 1);
+		/* Count paths with sum on the left and right. */
+        totalPaths += countPathsWithSum(node.left, targetSum, runningSum, pathCount);
+        totalPaths += countPathsWithSum(node.right, targetSum, runningSum, pathCount);
+
+        incrementHashTable(pathCount, runningSum, -1); // Remove runningSum
+        return totalPaths;
+    }
+    public static void incrementHashTable(HashMap<Integer, Integer> hashTable, int key, int delta) {
+        int newCount = hashTable.getOrDefault(key, 0) + delta;
+        if (newCount == 0) { // Remove when zero to reduce space usage
+            hashTable.remove(key);
+        } else {
+            hashTable.put(key, newCount);
+        }
+    }
 
     //You are given a binary tree in which each node contains a value. Design an algorithm to print all paths which
     //sum to a given value. The path does not need to start or end at the root or a leaf.
