1a14101cff685a9c370b6f947ed27429dd0b3f08
piyushkp <er.piyushpatel@gmail.com>
2017-02-14 18:28:40 -0800

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -2896,5 +2896,47 @@ public class Array {
         }
         return a[low];
     }
+    //find Maximum and minimum of an array using minimum number of comparisons
+    struct pair{
+        int min;
+        int max;
+    };
+    private pair getMinMax(int arr[], int n) {
+        struct pair minmax;
+        int i;
+  /* If array has even number of elements then initialize the first two elements as minimum and maximum */
+        if (n % 2 == 0) {
+            if (arr[0] > arr[1]) {
+                minmax.max = arr[0];
+                minmax.min = arr[1];
+            } else {
+                minmax.min = arr[0];
+                minmax.max = arr[1];
+            }
+            i = 2;  /* set the startung index for loop */
+        }
+    /* If array has odd number of elements then initialize the first element as minimum and maximum */
+        else {
+            minmax.min = arr[0];
+            minmax.max = arr[0];
+            i = 1;  /* set the startung index for loop */
+        }
+     /* In the while loop, pick elements in pair and compare the pair with max and min so far */
+        while (i < n - 1) {
+            if (arr[i] > arr[i + 1]) {
+                if (arr[i] > minmax.max)
+                    minmax.max = arr[i];
+                if (arr[i + 1] < minmax.min)
+                    minmax.min = arr[i + 1];
+            } else {
+                if (arr[i + 1] > minmax.max)
+                    minmax.max = arr[i + 1];
+                if (arr[i] < minmax.min)
+                    minmax.min = arr[i];
+            }
+            i += 2; /* Increment the index by 2 as two elements are processed in loop */
+        }
+        return minmax;
+    }
 
 }
