68ea0f1d103902e570f1e07a0f8a11b1638a35bf
piyushkp <er.piyushpatel@gmail.com>
2017-04-03 19:55:28 -0700

    Updated

--- src/code/ds/Threading.java
+++ src/code/ds/Threading.java
@@ -2,6 +2,7 @@ package code.ds;
 /**
  * Created by Piyush Patel.
  */
+import java.util.*;
 import java.util.HashMap;
 import java.util.concurrent.locks.*;
 public class Threading {
@@ -202,5 +203,60 @@ public class Threading {
             }
             notifyAll();
         }
+    }
+    //Merge Sort using Multi threading
+    public static void parallelMergeSort(int[] a, int NUM_THREADS){
+        if(NUM_THREADS <= 1){
+            mergeSort(a);
+            return;
+        }
+        int mid = a.length / 2;
+        int[] left = Arrays.copyOfRange(a, 0, mid);
+        int[] right = Arrays.copyOfRange(a, mid, a.length);
+
+        Thread leftSorter = mergeSortThread(left, NUM_THREADS);
+        Thread rightSorter = mergeSortThread(right, NUM_THREADS);
+        leftSorter.start();
+        rightSorter.start();
+        try {
+            leftSorter.join();
+            rightSorter.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        merge(left, right, a);
+    }
+
+    private static Thread mergeSortThread(int[] a, int NUM_THREADS){
+        return new Thread(){
+            @Override
+            public void run(){
+                parallelMergeSort(a, NUM_THREADS / 2);
+            }
+        };
+    }
+    public static void mergeSort(int[] a){
+        if(a.length <= 1) return;
+        int mid = a.length / 2;
+        int[] left = Arrays.copyOfRange(a, 0, mid);
+        int[] right = Arrays.copyOfRange(a, mid, a.length);
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(left, right, a);
+    }
+    private static void merge(int[] a, int[] b, int[] r){
+        int i = 0, j = 0, k = 0;
+        while(i < a.length && j < b.length){
+            if(a[i] < b[j])
+                r[k++] = a[i++];
+            else
+                r[k++] = b[j++];
+        }
+        while(i < a.length)
+            r[k++] = a[i++];
+        while(j < b.length)
+            r[k++] = b[j++];
     }
 }
