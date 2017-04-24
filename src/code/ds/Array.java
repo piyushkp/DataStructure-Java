424dde67c64d91e061967aa7999d839654358c69
piyushkp <er.piyushpatel@gmail.com>
2017-04-24 12:38:27 -0700

    Updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -2207,6 +2207,19 @@ public class Array {
         }
         return profit;
     }
+    //Say you have an array for which the ith element is the price of a given stock on day i.
+    // Design an algorithm to find the maximum profit. You may complete at most two transactions.
+    public int maxProfit3(int[] prices) {
+        int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
+        int release1 = 0, release2 = 0;
+        for(int i:prices){                              // Assume we only have 0 money at first
+            release2 = Math.max(release2, hold2+i);     // The maximum if we've just sold 2nd stock so far.
+            hold2    = Math.max(hold2,    release1-i);  // The maximum if we've just buy  2nd stock so far.
+            release1 = Math.max(release1, hold1+i);     // The maximum if we've just sold 1nd stock so far.
+            hold1    = Math.max(hold1,    -i);          // The maximum if we've just buy  1st stock so far.
+        }
+        return release2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
+    }
 
     //Given stock prices for certain days how to maximize profit by buying or selling with at most K transactions
     //Time complexity - O(number of transactions * number of days ^ 2) Space complexity - O(number of transcations * number of days)
