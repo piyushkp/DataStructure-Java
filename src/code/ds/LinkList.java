e54e864c29669eef0d5a6b9620c672ac1a4bef00
piyushkp <er.piyushpatel@gmail.com>
2017-05-03 14:11:53 -0700

    Updated

--- src/code/ds/LinkList.java
+++ src/code/ds/LinkList.java
@@ -965,4 +965,31 @@ public class LinkList {
             second = temp;
         }
     }
+    //Merge two sorted link list
+    Node MergeLists(Node list1, Node list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+        Node head;
+        if (list1.data < list2.data) {
+            head = list1;
+        } else {
+            head = list2;
+            list2 = list1;
+            list1 = head;
+        }
+        while (list1.next != null && list2 != null) {
+            if (list1.next.data <= list2.data) {
+                list1 = list1.next;
+            } else {
+                Node tmp = list1.next;
+                list1.next = list2;
+                list2 = tmp;
+            }
+        }
+        if (list1.next == null)
+            list1.next = list2;
+        return head;
+    }
 }
