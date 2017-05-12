bd327264348dd4df045e494e3fc863f7c5dc7150
piyushkp <er.piyushpatel@gmail.com>
2017-05-11 17:19:17 -0700

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -1709,31 +1709,45 @@ public class StringImp {
 
     /**
      * Dynamic programming technique for regex matching. Time and space = O(M * N)
+     *  1, If p.charAt(j) == s.charAt(i) :  dp[i][j] = dp[i-1][j-1];
+        2, If p.charAt(j) == '.' : dp[i][j] = dp[i-1][j-1];
+        3, If p.charAt(j) == '*':
+            here are two sub conditions:
+             1   if p.charAt(j-1) != s.charAt(i) : dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+             2   if p.charAt(i-1) == s.charAt(i) or p.charAt(i-1) == '.':
+             dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a
+             or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+             or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
      */
-    public boolean matchRegex(char[] text, char[] pattern) {
-        boolean T[][] = new boolean[text.length + 1][pattern.length + 1];
-        T[0][0] = true;
-        //Deals with patterns like a* or a*b* or a*b*c*
-        for (int i = 1; i < T[0].length; i++) {
-            if (pattern[i - 1] == '*') {
-                T[0][i] = T[0][i - 2];
+    public boolean matchRegex(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
             }
         }
-        for (int i = 1; i < T.length; i++) {
-            for (int j = 1; j < T[0].length; j++) {
-                if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
-                    T[i][j] = T[i - 1][j - 1];
-                } else if (pattern[j - 1] == '*') {
-                    T[i][j] = T[i][j - 2];
-                    if (pattern[j - 2] == '.' || pattern[j - 2] == text[i - 1]) {
-                        T[i][j] = T[i][j] | T[i - 1][j];
+        for (int i = 1 ; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j-1) == '.') {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if (p.charAt(j-1) == s.charAt(i-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if (p.charAt(j-1) == '*') {
+                    if (p.charAt(j-2) != s.charAt(i-1) && p.charAt(j-2) != '.') {
+                        dp[i][j] = dp[i][j-2];
+                    } else {
+                        dp[i][j] = (dp[i][j-1]||dp[i][j-2]||dp[i-1][j]);
                     }
-                } else {
-                    T[i][j] = false;
                 }
             }
         }
-        return T[text.length][pattern.length];
+        return dp[s.length()][p.length()];
     }
 
     //non-dynamic exponential complexity
