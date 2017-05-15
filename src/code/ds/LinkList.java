8d9a357d7638e4da4b1143160464f74b4eedc2f1
piyushkp <er.piyushpatel@gmail.com>
2017-05-15 15:19:33 -0700

    Updated

--- src/code/ds/LinkList.java
+++ src/code/ds/LinkList.java
@@ -1051,4 +1051,23 @@ public class LinkList {
             list1.next = list2;
         return head;
     }
+    //sorted linklist to BST
+    Node sortedListToBSTRecur(int n){
+        /* Base Case */
+        if (n <= 0)
+            return null;
+        /* Recursively construct the left subtree */
+        Node left = sortedListToBSTRecur(n / 2);
+        /* head_ref now refers to middle node, make middle node as root of BST*/
+        Node root = head;
+        // Set pointer to left subtree
+        root.prev = left;
+        /* Change head pointer of Linked List for parent recursive calls */
+        head = head.next;
+        /* Recursively construct the right subtree and link it with root. The number of nodes in right subtree  is
+           total nodes - nodes in left subtree - 1 (for root) */
+        root.next = sortedListToBSTRecur(n - n / 2 - 1);
+
+        return root;
+    }
 }
