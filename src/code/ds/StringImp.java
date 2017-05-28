92b08c624d7718f0f61502e5fb1e8dfd54c40296
piyushkp <er.piyushpatel@gmail.com>
2017-05-27 21:41:24 -0700

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -2948,24 +2948,30 @@ public class StringImp {
     private static final String[] big = {"thousand", "million", "billion"};
     public static String convertNumberToWords (int n) {
         if (n < 0) {
-            return "minus " + convertNumberToWords(-n); }
+            return "minus " + convertNumberToWords(-n);
+        }
         if (n <= 999) {
-            return convert999(n); }
+            return convert999(n);
+        }
         String s = null;
         int t = 0;
         while (n > 0) {
             if (n % 1000 != 0) {
                 String s2 = convert999(n % 1000);
                 if (t > 0) {
-                    s2 = s2 + " " + big[t-1]; }
+                    s2 = s2 + " " + big[t - 1];
+                }
                 if (s == null) {
-                    s = s2; }
-                else {
-                    s = s2 + ", " + s; }}
+                    s = s2;
+                } else {
+                    s = s2 + ", " + s;
+                }
+            }
             n /= 1000;
-            t++; }
-        return s; }
-
+            t++;
+        }
+        return s;
+    }
     // Range 0 to 999.
     private static String convert999 (int n) {
         String s1 = one[n / 100] + " hundred";
