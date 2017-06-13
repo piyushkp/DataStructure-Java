59389efbb88106a50233e4bd5fe8e2c23b270694
piyushkp <er.piyushpatel@gmail.com>
2017-06-13 16:01:56 -0700

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -512,23 +512,9 @@ public class StringImp {
     public static boolean wordBreak(String s, Set<String> dict) {
         boolean[] f = new boolean[s.length() + 1];
         f[0] = true;
-        /* First DP
-        for(int i = 1; i <= s.length(); i++){
-            for(String str: dict){
-                if(str.length() <= i){
-                    if(f[i - str.length()]){
-                        if(s.substring(i-str.length(), i).equals(str)){
-                            f[i] = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }*/
-        //Second DP
-        for(int i=1; i <= s.length(); i++){
-            for(int j=0; j < i; j++){
-                if(f[j] && dict.contains(s.substring(j, i))){
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (f[j] && dict.contains(s.substring(j, i))) {
                     f[i] = true;
                     break;
                 }
