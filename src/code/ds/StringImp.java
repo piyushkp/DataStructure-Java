b9ca7f791d1ce83805d3203e2d475ec4b5ebc8fd
piyushkp <er.piyushpatel@gmail.com>
2017-06-15 17:07:21 -0700

    Updated

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -1743,10 +1743,7 @@ public class StringImp {
         }
         for (int i = 1 ; i <= s.length(); i++) {
             for (int j = 1; j <= p.length(); j++) {
-                if (p.charAt(j-1) == '.') {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                if (p.charAt(j-1) == s.charAt(i-1)) {
+                if (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1)) {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 if (p.charAt(j-1) == '*') {
