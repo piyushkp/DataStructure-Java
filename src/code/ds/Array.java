71f5ab6ca1994134be0c784df5b25c838b299471
piyushkp <er.piyushpatel@gmail.com>
2017-07-21 18:42:27 -0700

    updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -3665,6 +3665,21 @@ public class Array {
             }
         }
     }
+    // first bad commit version
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    boolean isBadVersion(int n){return true;}
 
 
 }
