1c7ad24750ebcc8b77041c77a4d53dd37db0b073
piyush <er.piyushpatel@gmail.com>
2017-09-01 14:07:34 -0700

    test

--- src/code/ds/StringImp.java
+++ src/code/ds/StringImp.java
@@ -166,7 +166,7 @@ public class StringImp {
         }
         return false;
     }
-    
+
     //Write an algorithm to determine if all of the delimiters in an expression are matched and closed
     //{(abc)22}[14(xyz)2] should pass
     static boolean isBalanced(String input) {
--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -2315,7 +2315,8 @@ public class Tree {
     }
     /*Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
       Calling next() will return the next smallest number in the BST. Note: next() and hasNext() should run in
-      average O(1) time and uses O(h) memory, where h is the height of the tree. */
+      average O(1) time and uses O(h) memory, where h is the height of the tree.
+      For next() to be O(1) space and time O(n) use morris traversal */
     class BSTIterator {
         private Stack<Node> stack = new Stack<>();
         public BSTIterator(Node root) {
