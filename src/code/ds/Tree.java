bf63975e48812e004b95418a62ed1218e032b9a4
piyush <er.piyushpatel@gmail.com>
2018-07-17 10:21:25 -0700

    updated

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -2340,4 +2340,40 @@ public class Tree {
             }
         }
     }
+    /* upside down tree: Given a binary tree where all the right nodes are either leaf nodes with a sibling
+    (a left node that shares the same parent node) or empty, flip it upside down and turn it into a
+    tree where the original right nodes turned into left leaf nodes. Return the new root.
+        For example:
+        Given a binary tree {1,2,3,4,5},
+        1
+        / \
+        2 3
+        / \
+        4 5
+
+        return the root of the binary tree [4,5,2,#,#,3,1].
+        4
+        / \
+        5 2
+          / \
+         3 1
+     */
+    public Node upsideDownBinaryTree(Node root) {
+        Node curr = root;
+        Node next = null;
+        Node temp = null;
+        Node prev = null;
+        while(curr != null) {
+            next = curr.left;
+            // swapping nodes now, need temp to keep the previous right child
+            curr.left = temp;
+            temp = curr.right;
+            curr.right = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
 }
