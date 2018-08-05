773b6d892478b2bd4294a18cb179b8de0bbc255b
piyush <er.piyushpatel@gmail.com>
2018-08-04 18:38:51 -0700

    code updated

--- src/code/ds/Array.java
+++ src/code/ds/Array.java
@@ -4113,5 +4113,31 @@ public class Array {
     B = [50, 12, 32, 46, 28]
     output = [1, 4, 3, 2, 0]   */
 
+  /*Given a sorted array of integers nums and integer values a, b and c. Apply a function of the
+  form f(x) = ax2 + bx + c to each element x in the array. The returned array must be in sorted order.
+  Expected time complexity: O(n)*/
+  public static int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+    int n = nums.length;
+    int[] sorted = new int[n];
+    int i = 0, j = n - 1;
+    int index = a >= 0 ? n - 1 : 0;
+    while (i <= j) {
+      if (a >= 0) {
+        sorted[index--] =
+            quad(nums[i], a, b, c) >= quad(nums[j], a, b, c) ? quad(nums[i++], a, b, c)
+                : quad(nums[j--], a, b, c);
+      } else {
+        sorted[index++] =
+            quad(nums[i], a, b, c) >= quad(nums[j], a, b, c) ? quad(nums[j--], a, b, c)
+                : quad(nums[i++], a, b, c);
+      }
+    }
+    return sorted;
+  }
+
+  private static int quad(int x, int a, int b, int c) {
+    return a * x * x + b * x + c;
+  }
+
 
 }
