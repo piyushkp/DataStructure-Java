c53e2ce63c4209ab0fa12834b83f5c3e8e3afd37
piyush <er.piyushpatel@gmail.com>
2018-09-11 12:34:45 -0700

    question added

--- src/code/ds/Tree.java
+++ src/code/ds/Tree.java
@@ -2123,6 +2123,44 @@ public class Tree {
     return root;
   }
 
+  // Serialize and Deserialize BST
+  private String splitter = ",";
+  // Encodes a tree to a single string.
+  public String serialize(Node root) {
+    StringBuilder sb = new StringBuilder();
+    buildString(root, sb);
+    return sb.toString();
+  }
+
+  private void buildString(Node root, StringBuilder sb) {
+    if (root == null) return;
+    sb.append(root.data).append(splitter);
+    buildString(root.left, sb);
+    buildString(root.right, sb);
+  }
+
+  // Decodes your encoded data to tree.
+  public Node deserialize(String data) {
+    if (data.length() == 0) return null;
+    int[] pos = new int[1];
+    pos[0] = 0;
+    return buildTree(data.split(splitter), pos, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private Node buildTree(String[] nodes, int[] pos, int min, int max) {
+    if (pos[0] == nodes.length) return null;
+
+    int val = Integer.valueOf(nodes[pos[0]]);
+    if (val < min || val > max) return null; // Go back if we are over the boundary
+    Node cur = new Node(val);
+
+    pos[0]++; // update current position
+    cur.left = buildTree(nodes, pos, min, val);
+    cur.right = buildTree(nodes, pos, val, max);
+    return cur;
+  }
+
+
   /**
    * Given a sorted (increasing order) array with unique integer elements, write an algorithm to
    * create a binary search tree with minimal height.
